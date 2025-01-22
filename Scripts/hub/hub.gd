ae58a8128f410d3cb44fc31784bdf7e3f781fbe5
Cloenk <165511519+Cloenk@users.noreply.github.com>
2025-01-22 15:40:07 +0100

    hub with clouds

---
+++ Scripts/hub/hub.gd
@@ -0,0 +1,10 @@
+extends Node3D
+
+@onready var bottem_clouds = $BottemClouds
+@onready var top_clouds = $TopClouds
+
+func _process(delta):
+	bottem_clouds.mesh.get_material().uv1_offset.x += 0.03 * delta
+	bottem_clouds.mesh.get_material().uv1_offset.z += 0.03 * delta
+	top_clouds.mesh.get_material().uv1_offset.x += 0.05 * delta
+	top_clouds.mesh.get_material().uv1_offset.z -= 0.05 * delta
