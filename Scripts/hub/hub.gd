7e588346176e1a6fcd5a38d3b0787640388a91b0
Cloenk <165511519+Cloenk@users.noreply.github.com>
2025-01-22 19:02:17 +0100

    tp when fall

--- Scripts/hub/hub.gd
+++ Scripts/hub/hub.gd
@@ -3,6 +3,7 @@ extends Node3D
 @onready var bottem_clouds = $Clouds/BottemClouds
 @onready var middle_clouds = $Clouds/MiddleClouds
 @onready var top_clouds = $Clouds/TopClouds
+@onready var player = $Player
 
 func _process(delta):
 	bottem_clouds.mesh.get_material().uv1_offset.x += 0.03 * delta
@@ -11,3 +12,6 @@ func _process(delta):
 	middle_clouds.mesh.get_material().uv1_offset.z -= 0.05 * delta
 	top_clouds.mesh.get_material().uv1_offset.x -= 0.05 * delta
 	top_clouds.mesh.get_material().uv1_offset.z += 0.05 * delta
+	
+	if player.position.y < -11:
+		player.position = Vector3(0,26,0)
