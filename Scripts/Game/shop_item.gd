05a58937ea80bc59cd62e209026251e0cf159c69
Cloenk <165511519+Cloenk@users.noreply.github.com>
2025-01-29 22:00:54 +0100

    shop works now yippers

--- Scripts/Game/shop_item.gd
+++ Scripts/Game/shop_item.gd
@@ -1,6 +1,12 @@
 extends Node2D
 @onready var game = $"../.."
+@onready var background = $Background
+@onready var sprite = $Icon
+@onready var hover = $Hover
 @onready var shop = $".."
+@onready var item_name = $Hover/ItemName
+@onready var description = $Hover/Description
+@onready var bought_sprites = $boughtSprites
 @onready var price = $Price
 
 @export var upgrade: Upgrade
@@ -16,17 +22,31 @@ func randomUpgrade():
 	if shop.availableUpgrades.size() > 0:
 		upgrade = shop.availableUpgrades.pick_random()
 		shop.availableUpgrades.erase(upgrade)
-		price.text = upgrade.name
+		if upgrade != null:
+			price.text = str(upgrade.Price)
+			item_name.text = upgrade.name
+			description.text = upgrade.description
+			sprite.texture = upgrade.Icon
+			background.texture = upgrade.Icon
 
 func stopShop():
 	if bought == false:
 		shop.availableUpgrades.append(upgrade)
-	bought = false
-	upgrade == null
+	upgrade = null
 	price.text = "Out of stock"
+	sprite.texture = null
+	background.texture = null
+	bought_sprites.hide()
 
 func _on_buy_button_pressed():
 	if shop.gold >= upgrade.Price and bought == false:
 		bought = true
+		shop.availableUpgrades.erase(upgrade)
 		shop.gold -= upgrade.Price
 		shop.player.upgrades.AcquireItem(upgrade)
+		bought_sprites.show()
+
+func _on_buy_button_mouse_entered():
+	hover.show()
+func _on_buy_button_mouse_exited():
+	hover.hide()
--- Scripts/Upgrades/Upgrade.gd
+++ Scripts/Upgrades/Upgrade.gd
@@ -2,6 +2,7 @@ extends Resource
 class_name Upgrade
 
 @export var name: String
+@export var description: String
 @export var Icon: Texture
 @export var Price: float
 @export var ID: int
